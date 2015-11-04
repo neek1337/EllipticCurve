7695a339fd1e68c85ec6ef2bb29ab0dcf4d0b54d
neek1337 <vdo1337@gmail.com>
2015-11-04 15:17:57 +0300

    q

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,28 @@
 import javafx.util.Pair;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(System.in);
         int bitSize = scanner.nextInt();
-        BigInteger i, p, h = null;
+        BigInteger i, p, h1 = null, h2 = null;
         ComplexNumber decomposition = null;
         do {
             p = generateP(bitSize);
             i = findingI1(p);
-            if (i == null) {
-                continue;
-            }
             decomposition = gcd(new ComplexNumber(p), new ComplexNumber(i, BigInteger.ONE));
-
-            h = p.add(BigInteger.ONE).subtract(decomposition.getR().multiply(new BigInteger("2")));
-        } while (!check(h, p));
+            h1 = p.add(BigInteger.ONE).subtract(decomposition.getR().multiply(new BigInteger("2")).abs());
+        } while (!check(h1, p));
         System.out.println(decomposition);
         System.out.println("p = " + p);
         System.out.println("I = " + i);
-        System.out.println("Порядок группы = " + h);
+        System.out.println("Порядок группы = " + h1);
         BigInteger a = null;
         Pair<BigInteger, BigInteger> p0 = null;
         Pair<BigInteger, BigInteger> sum = null;
@@ -34,34 +33,66 @@ public class Main {
                 a = p0.getValue().multiply(p0.getValue())
                         .subtract(p0.getKey().multiply(p0.getKey()).
                                 multiply(p0.getKey())).multiply(inverse(p, p0.getKey())).mod(p);
-            } while (isQuadraticResidue(p.subtract(a), h));
+            } while (isQuadraticResidue(p.subtract(a), h1));
           /*  System.out.println("a = " + a);
             System.out.println("x0 = " + p0.getKey());
             System.out.println("y0 = " + p0.getValue());    */
-            sum = new Pair<BigInteger, BigInteger>(BigInteger.ONE, BigInteger.ONE);
-            for (int j = 0; j < h.intValue(); j++) {
-                sum = calcSum(sum, p0, p);
+            sum = new Pair<BigInteger, BigInteger>(BigInteger.ZERO, BigInteger.ZERO);
+
+            for (int j = 0; j < h1.intValue(); j++) {
+                sum = calcSum(sum, p0, p, a);
                 if (sum == null) {
                     break;
                 }
             }
         } while (sum != null);
         System.out.println("a = " + a);
-        System.out.print("Генератор : x0 = " + p0.getKey());
-        System.out.println(";y0 = " + p0.getValue());
+        System.out.print("Генератор : x = " + p0.getKey());
+        System.out.println(";y = " + p0.getValue());
         System.out.println("y^2 = x^3 + " + a + "*x");
+        sum = new Pair<BigInteger, BigInteger>(BigInteger.ZERO, BigInteger.ZERO);
+        PrintWriter printWriterX = new PrintWriter(new File("x.txt"));
+        for (int j = 0; j < h1.intValue(); j++) {
+            if (sum != null) {
+                printWriterX.println(sum.getKey());
+            }
+            sum = calcSum(sum, p0, p, a);
+        }
 
+        PrintWriter printWriterY = new PrintWriter(new File("y.txt"));
+        sum = new Pair<BigInteger, BigInteger>(BigInteger.ZERO, BigInteger.ZERO);
+
+        System.out.println("_________________________________");
+        for (int j = 0; j < h1.intValue(); j++) {
+            if (sum != null) {
+                printWriterY.println(sum.getValue());
+            }
+            sum = calcSum(sum, p0, p, a);
+        }
+        printWriterX.flush();
+        printWriterY.flush();
     }
+
 
     public static BigInteger inverse(BigInteger p, BigInteger a) {
         return a.modPow(p.subtract(new BigInteger("2")), p);
     }
 
-    public static Pair<BigInteger, BigInteger> calcSum(Pair<BigInteger, BigInteger> a1, Pair<BigInteger, BigInteger> a2, BigInteger p) {
-        if (a1.getKey().equals(a2.getKey())) {
-            return null;
+    public static Pair<BigInteger, BigInteger> calcSum(Pair<BigInteger, BigInteger> a1, Pair<BigInteger, BigInteger> a2, BigInteger p, BigInteger a) {
+        BigInteger lambda = null;
+        if (a1 == null) {
+            return a2;
         }
-        BigInteger lambda = a2.getValue().subtract(a1.getValue()).multiply(inverse(p, a2.getKey().subtract(a1.getKey()))).mod(p);
+        if (a2 == null) {
+            return a1;
+        }
+        if (a1.equals(a2)) {
+            lambda = new BigInteger("3").multiply(a1.getKey()).multiply(a1.getKey()).add(a).multiply(inverse(p, new BigInteger("2").multiply(a1.getValue())));
+        } else if (a1.getKey().equals(a2.getKey())) {
+            return null;
+        } else {
+            lambda = (a2.getValue().subtract(a1.getValue())).multiply(inverse(p, a2.getKey().subtract(a1.getKey()))).mod(p);
+        }
         BigInteger x3 = lambda.multiply(lambda).subtract(a2.getKey()).subtract(a1.getKey()).mod(p);
         BigInteger y3 = lambda.multiply(a1.getKey().subtract(x3)).subtract(a1.getValue()).mod(p);
         return new Pair<BigInteger, BigInteger>(x3, y3);
